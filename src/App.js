00953ebe2158efdbdc4e6272235484df72415731
Annie Huang <annie.hn@gmail.com>
2021-02-20 18:04:14 +1100

    Add <Button> and <Container>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import styled from 'styled-components'
+import {Button} from './components/Button';
+
+const Container = styled.div`
+  text-align: center;
+`
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <Button>Normal Button</Button>
+        <Button primary>Primary Button</Button>
+      </Container>
     </div>
   );
 }
